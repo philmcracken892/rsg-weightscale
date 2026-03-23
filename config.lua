5bc72a599b28eeba0e131911778c1608503afc9e
philmcracken892 <MARC.KELLETT@GOOGLEMAIL.COM>
2026-03-23 08:12:27 +0000

    Refactor config to support multiple scale models

--- config.lua
+++ config.lua
@@ -1,6 +1,9 @@
 Config = {}
 
-Config.ScaleModel = `p_drmedscale03x`
+Config.ScaleModels = {
+    `p_drmedscale03x`,
+    `p_drmedscale02x`
+}
 Config.InteractDistance = 1.5
 Config.WeightUnit = "lbs"
 Config.LbsToKg = 0.453592

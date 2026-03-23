325be02b000cc1081fd4016712df38e42f236248
philmcracken892 <MARC.KELLETT@GOOGLEMAIL.COM>
2026-03-23 08:13:44 +0000

    Implement weight check and slip generation logic

--- server/server.lua
+++ server/server.lua
@@ -1,9 +1,9 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 
--- Store player weight slip data
+
 local playerWeightSlips = {}
 
--- Handle weight check from scale
+
 RegisterNetEvent('rsg-weightscale:server:checkWeight', function()
     local src = source
     local Player = RSGCore.Functions.GetPlayer(src)
@@ -13,10 +13,10 @@ RegisterNetEvent('rsg-weightscale:server:checkWeight', function()
     local playerName = Player.PlayerData.charinfo.firstname .. ' ' .. Player.PlayerData.charinfo.lastname
     local dateStr = os.date("%B %d, 1899")
     
-    -- Random weight every time (80-250 lbs)
+   
     local weight = math.random(80, 250)
     
-    -- Store weight slip data for this player
+   
     local visaId = Player.PlayerData.citizenid
     playerWeightSlips[visaId] = {
         weight = weight,
@@ -25,38 +25,38 @@ RegisterNetEvent('rsg-weightscale:server:checkWeight', function()
         date = dateStr
     }
     
-    -- Give weight slip item if enabled
+   
     if Config.GiveWeightSlip then
-        -- Check if player already has a weight slip
+      
         local hasSlip = Player.Functions.GetItemByName(Config.WeightSlipItem)
         if hasSlip then
-            -- Remove old slip
+           
             Player.Functions.RemoveItem(Config.WeightSlipItem, 1)
         end
         
-        -- Add new weight slip with metadata
+      
         Player.Functions.AddItem(Config.WeightSlipItem, 1, false, playerWeightSlips[visaId])
         TriggerClientEvent('inventory:client:ItemBox', src, RSGCore.Shared.Items[Config.WeightSlipItem], "add")
     end
     
-    -- Show weight to player
+   
     TriggerClientEvent('rsg-weightscale:client:showWeight', src, weight, playerName, false, dateStr)
 end)
 
--- Use weight slip item
+
 RSGCore.Functions.CreateUseableItem(Config.WeightSlipItem, function(source, item)
     local src = source
     local Player = RSGCore.Functions.GetPlayer(src)
     
     if not Player then return end
     
-    -- Get slip data from item metadata
+    
     local slipData = item.info
     
     if slipData and slipData.weight then
         TriggerClientEvent('rsg-weightscale:client:showWeightSlip', src, slipData)
     else
-        -- Fallback to stored data
+       
         local visaId = Player.PlayerData.citizenid
         if playerWeightSlips[visaId] then
             TriggerClientEvent('rsg-weightscale:client:showWeightSlip', src, playerWeightSlips[visaId])
@@ -66,7 +66,7 @@ RSGCore.Functions.CreateUseableItem(Config.WeightSlipItem, function(source, item
     end
 end)
 
--- Admin command to set player weight
+
 RSGCore.Commands.Add('setweight', 'Set player weight', {
     {name = 'id', help = 'Player ID'}, 
     {name = 'weight', help = 'Weight'}
